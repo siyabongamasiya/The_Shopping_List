437d5255e347f92342e214bb496192d191c055cb
siyabonga <siyabongakhanyile76@gmail.com>
2026-01-12 11:05:40 +0200

    fix:Removed the react-native-reanimated import from _layout.tsx.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import "react-native-reanimated";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
